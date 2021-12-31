990d958d037ae475c43a350ebd67e29bee05ba4e
prongbang <prongbang@gmail.com>
2021-12-31 23:09:25 +0700

    initial consumer

---
+++ consume/src/main/kotlin/consumer/KafkaConsumer.kt
@@ -0,0 +1,77 @@
+package com.prongbang.consumer
+
+import java.util.Properties
+import org.apache.http.HttpHost
+
+import com.fasterxml.jackson.databind.JsonNode
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.connect.json.JsonDeserializer
+import org.elasticsearch.ElasticsearchException
+
+import org.elasticsearch.action.bulk.BulkRequest
+import org.elasticsearch.action.index.IndexRequest
+import org.elasticsearch.client.RestHighLevelClient
+import org.elasticsearch.client.RestClient
+
+class KafkaConsumer {
+
+    companion object {
+        fun newInstance(): KafkaConsumer = KafkaConsumer()
+    }
+
+    fun run() {
+        val bootstrapServersConfig = System.getenv("BOOTSTRAP_SERVERS_CONFIG")
+        val groupIdConfig = System.getenv("GROUP_ID_CONFIG")
+        val topic = "user-messages"
+
+        val props = Properties()
+        with(props) {
+            put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServersConfig)
+            put(ConsumerConfig.GROUP_ID_CONFIG, groupIdConfig)
+            put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer().javaClass.name)
+            put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer().javaClass.name)
+            put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
+            put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false")
+        }
+
+        val esHost = System.getenv("ES_HOST")
+        val esPort = System.getenv("ES_PORT").toInt()
+
+        val client = RestHighLevelClient(RestClient.builder(HttpHost(esHost, esPort, "http")))
+
+        val consumer = KafkaConsumer<String, JsonNode>(props)
+        consumer.subscribe(listOf(topic))
+
+        while (true) {
+            val consumerRecords = consumer.poll(1000)
+
+            if (consumerRecords.count() == 0) {
+                continue
+            }
+
+            val bulkRequest = BulkRequest()
+
+            consumerRecords.forEach {
+                val id = it.value().get("id").asText()
+                val message = it.value().get("message").asText()
+                val jsonMap = mapOf("id" to id, "message" to message)
+
+                // Your additional business logic
+
+                bulkRequest.add(IndexRequest("user-messages", "doc", id).source(jsonMap))
+            }
+
+            try {
+                client.bulk(bulkRequest)
+                consumer.commitAsync()
+                println("Indexed successfully")
+            } catch (e: ElasticsearchException) {
+                // handle when something goes wrong.
+                println(e)
+            }
+        }
+        consumer.close()
+    }
+}
